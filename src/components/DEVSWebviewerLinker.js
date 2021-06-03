89b4ff93794dbcb25f7a1e58d5e3ea4f7400f6ab
tanvirislamcu <tanvirislam@cunet.carleton.ca>
2021-06-03 08:47:36 -0400

    cards without connetions will now show a warning message

--- src/components/DEVSWebviewerLinker.js
+++ src/components/DEVSWebviewerLinker.js
@@ -6,6 +6,7 @@ export class DEVSWebviewerLinker extends Component {
     constructor(elem, props) {
         super(elem, props);
         this.state = {
+            buttons: [],
             jsonContent: null,
             selectedSvgElements: {},
             currentButtonPicker: '',
@@ -16,10 +17,11 @@ export class DEVSWebviewerLinker extends Component {
                 ports: 'output ports',
                 links: 'links',
             },
+            svgIdMap: new Set()
         }
     }
 
-    getId = id => `#${id}`
+    parseId = id => `#${id}`
 
     getJsonContent = (id) => {
         const [key, str_index] = id.split('-');
@@ -33,7 +35,14 @@ export class DEVSWebviewerLinker extends Component {
         if (currentCardId) {
             const jsonElement = this.getJsonContent(this.state.currentCardId)
             jsonElement.svg = Object.keys(selectedSvgElements).map(id => id)
-            // TODO
+            const warningIconId = `warning-icon-${currentCardId}`
+            if (jsonElement.svg.length === 0) {
+                document.getElementById(warningIconId).classList.remove('invisible')
+                document.getElementById(warningIconId).classList.add('visible')
+            } else {
+                document.getElementById(warningIconId).classList.remove('visible')
+                document.getElementById(warningIconId).classList.add('invisible')
+            }
         }
     }
 
@@ -138,6 +147,16 @@ export class DEVSWebviewerLinker extends Component {
         }
     }
 
+    showIcon = (svg, id) => {
+        const hasAssociation = svg.some(id => this.state.svgIdMap.has(id))
+        return `
+            <svg id="${id}" style="position:absolute;right:3;bottom:3" class="${hasAssociation ? 'invisible' : 'visible'} warning-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle" viewBox="0 0 16 16">
+              <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z"/>
+              <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995z"/>
+            </svg>
+        `
+    }
+
     onJsonButtonPicker = (cardsContainer, buttons, key, jsonContent) => {
         if (key !== this.state.currentButtonPicker) {
             this.clearSvgSelections()
@@ -152,9 +171,13 @@ export class DEVSWebviewerLinker extends Component {
                         const slice = jsonContent[key]
                         if (Array.isArray(slice)) {
                             slice.forEach((item, index) => {
+                                const jsonElem = jsonContent[key][index]
                                 const card = this.addHTMLTo(
                                     cardsContainer,
-                                    `<div id="${key}-${index}" class="card m-1 p-4 dwl-pointer dwl-card">${this.getCardContent(key, item)}</div`
+                                    `<div style="position:relative" id="${key}-${index}" class="card m-1 p-4 dwl-pointer dwl-card">
+                                        ${this.getCardContent(key, item)}
+                                        ${this.showIcon(Array.isArray(jsonElem.svg) ? jsonElem.svg : [], `warning-icon-${key}-${index}`)}
+                                    </div`
                                 )
                                 card.addEventListener('click', () => {
                                     this.onCardClick(card)
@@ -167,9 +190,13 @@ export class DEVSWebviewerLinker extends Component {
                 const slice = jsonContent[key]
                 if (Array.isArray(slice)) {
                     slice.forEach((item, index) => {
+                        const jsonElem = jsonContent[key][index]
                         const card = this.addHTMLTo(
                             cardsContainer,
-                            `<div id="${key}-${index}" class="card m-1 p-4 dwl-pointer dwl-card">${this.getCardContent(key, item)}</div`
+                            `<div style="position:relative" id="${key}-${index}" class="card m-1 p-4 dwl-pointer dwl-card">
+                                ${this.getCardContent(key, item)}
+                                ${this.showIcon(Array.isArray(jsonElem.svg) ? jsonElem.svg : [], `warning-icon-${key}-${index}`)}
+                            </div`
                         )
                         card.addEventListener('click', () => {
                             this.onCardClick(card)
@@ -198,11 +225,15 @@ export class DEVSWebviewerLinker extends Component {
             .each(function(_, index) {
                 const existingId = d3.select(this).attr('id');
                 if (!existingId) {
-                    d3.select(this).attr('id', `e-${index}`);
+                    const id =`e-${index}` 
+                    d3.select(this).attr('id', id);
+                    self.state.svgIdMap.add(`#${id}`)
+                } else {
+                    self.state.svgIdMap.add(`#${existingId}`)
                 }
             })
             .on('click', function() {
-                const id = self.getId(d3.select(this).attr('id'))
+                const id = self.parseId(d3.select(this).attr('id'))
                 if (id in self.state.selectedSvgElements) {
                     const selections = { ...self.state.selectedSvgElements };
                     d3.select(this).attr('stroke', selections[id].stroke);
@@ -218,7 +249,7 @@ export class DEVSWebviewerLinker extends Component {
                 self.createAssociation()
             })
             .each(function() {
-                const id = self.getId(d3.select(this).attr('id'))
+                const id = self.parseId(d3.select(this).attr('id'))
                 if (id in self.state.selectedSvgElements) {
                     d3.select(this).attr('stroke', 'tomato');
                 }
@@ -245,7 +276,10 @@ export class DEVSWebviewerLinker extends Component {
                 }, false);
             }
         })
-        buttons[0].click()
+        this.setState(s => ({
+            ...s,
+            buttons,
+        }))
     }
 
     render = async () => {
@@ -259,6 +293,7 @@ export class DEVSWebviewerLinker extends Component {
         const svgContainer = this.addHTMLTo(container, '<div id="svg-container" class="card m-1 h-100 w-100" style="position:relative;" />')
         this.renderJson(jsonContainer, jsonContent)
         this.renderSvg(svgContainer, svgContent)
+        this.state.buttons[0].click()
     }
 
 }
