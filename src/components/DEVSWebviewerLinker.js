506852b57e74d4491796aa5696c1cfce05d30b8b
tanvirislamcu <tanvirislam@cunet.carleton.ca>
2021-06-07 15:18:07 -0400

    remove style tags

--- src/components/DEVSWebviewerLinker.js
+++ src/components/DEVSWebviewerLinker.js
@@ -190,7 +190,7 @@ export class DEVSWebviewerLinker extends Component {
     showIcon = (svg, id) => {
         const hasAssociation = svg.some(id => this.state.svgIdMap.has(id))
         return `
-            <svg id="${id}" style="position:absolute;right:3;bottom:3" class="${hasAssociation ? 'invisible' : 'visible'} warning-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle" viewBox="0 0 16 16">
+            <svg id="${id}" class="${hasAssociation ? 'invisible' : 'visible'} warning-icon position-absolute end-0 bottom-0" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle" viewBox="0 0 16 16">
               <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z"/>
               <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995z"/>
             </svg>
@@ -218,7 +218,7 @@ export class DEVSWebviewerLinker extends Component {
                                 }
                                 const card = this.addHTMLTo(
                                     cardsContainer,
-                                    `<div style="position:relative" id="${key}-${index}" class="card m-1 p-4 dwl-pointer dwl-card">
+                                    `<div id="${key}-${index}" class="card m-1 p-4 dwl-pointer dwl-card">
                                         ${this.getCardContent(key, item)}
                                         ${this.showIcon(Array.isArray(jsonElem.svg) ? jsonElem.svg : [], `warning-icon-${key}-${index}`)}
                                     </div`
@@ -243,7 +243,7 @@ export class DEVSWebviewerLinker extends Component {
                         }
                         const card = this.addHTMLTo(
                             cardsContainer,
-                            `<div style="position:relative" id="${key}-${index}" class="card m-1 p-4 dwl-pointer dwl-card">
+                            `<div id="${key}-${index}" class="card m-1 p-4 dwl-pointer dwl-card">
                                 ${this.getCardContent(key, item)}
                                 ${this.showIcon(Array.isArray(jsonElem.svg) ? jsonElem.svg : [], `warning-icon-${key}-${index}`)}
                             </div`
@@ -397,8 +397,8 @@ export class DEVSWebviewerLinker extends Component {
         this.state.originalJsonContent = this.clone(jsonContent);
         const container = this.addHTML("<div class='d-flex flex-row h-100 w-100' />")
         const jsonContainer = this.addHTMLTo(container, '<div id="json-container" class="d-flex flex-column card h-100 w-100" />')
-        const svgContainer = this.addHTMLTo(container, '<div id="svg-container" class="card m-1 h-100 w-100" style="position:relative;" />')
-        const utilityButtons = this.addHTMLTo(svgContainer, '<div style="position:absolute;right:0"/>')
+        const svgContainer = this.addHTMLTo(container, '<div id="svg-container" class="card m-1 h-100 w-100 position-relative" />')
+        const utilityButtons = this.addHTMLTo(svgContainer, '<div class="position-absolute end-0"/>')
         this.addHTMLTo(
             utilityButtons,
             `<button type="button" class="btn btn-primary m-1" data-button-type="reset">Reset</button>`
